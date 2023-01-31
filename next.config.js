4a6f572525100f9d8d540b6f878137b6c7b73135
dickhfchan <dickhfchan@gmail.com>
2023-01-31 14:11:53 +0800

    Update next.config.js
    remove the cross orgin

--- next.config.js
+++ next.config.js
@@ -1,24 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 
-module.exports = {
-  reactStrictMode: true,
-  headers() {
-    return [
-      {
-        source: '/',
-        headers: [
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp'
-          },
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin'
-          }
-        ]
-      }
-    ];
-  }
-};
+module.exports = nextConfig
+
 
